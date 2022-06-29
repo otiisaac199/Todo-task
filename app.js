8182deaee59e17440a00f72328938c7a9dcc3ac2
Isaac <otichinazaisaac19@gmail.com>
2022-06-29 16:29:50 +0100

    fixed the search bug

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const createTask = document.querySelector(".cursor");
 
 const cc = addTaskbtn.forEach((el) => {
   el.addEventListener("click", () => {
+    addTaskInput.value = "";
     modalCont.classList.add("active");
     addtaskModal.classList.add("active");
   });
@@ -34,10 +35,16 @@ const todoTaskContent = document.querySelector(".todo-task-content");
 
 const preTaskMessage = document.querySelector(".pre-task-message");
 
+addTaskInput.setAttribute("required", true);
+
+let mainerID;
+
 createTask.addEventListener("click", createTaskFunc);
 function createTaskFunc() {
   addTaskInputValue = addTaskInput.value;
-  if (addTaskInputValue == " ") alert("Please, Create a Task");
+
+  addTaskInputValue.trim();
+  if (!addTaskInputValue) alert("Please, Create a Task");
   else {
     // assignments of variables needed
     preTaskMessage.style.display = "none";
@@ -71,22 +78,40 @@ function createTaskFunc() {
     deleteIcon.addEventListener("click", () => {
       mainTodoTask.style.display = "none";
     });
-  }
-
-  const search = document.querySelector(".search");
-  const searchInput = document.querySelector(".searchinput");
-
-  search.addEventListener("click", () => {
-    searchInput.classList.toggle("active");
     const mainer = [...document.querySelectorAll(".main-todo-task")];
     console.log(mainer);
-
-    const todos = mainer.filter((el) => {
-      // return search.value == taskTxt.textContent;
-      console.log("Filtering");
-    });
-  });
+    mainerID = mainer;
+  }
 }
+
+const search = document.querySelector(".search");
+const searchInput = document.querySelector(".searchinput");
+
+search.addEventListener("click", () => {
+  searchInput;
+  searchInput.classList.toggle("active");
+  console.log(mainerID);
+
+  searchInput.addEventListener("keyup", (e) => {
+    searchInputValue = searchInput.value;
+    const fd = mainerID.filter((each_el) => {
+      let each = each_el.childNodes[0].childNodes[1].textContent;
+      if (each.toLowerCase().includes(searchInputValue)) {
+        return each_el;
+      }
+    });
+    let fdd = fd.map((each) => {
+      return `<div class="main-todo-task">
+        <div class="check-box-div">
+         <input type="checkbox" class="checkbox" />
+         <p class="task-txt">${each.textContent}</p>
+        </div>
+        <i class="bi bi-trash3"></i>
+     </div>`;
+    });
+    todoTaskContent.innerHTML = fdd;
+  });
+});
 
 // The Enter key button click to run the createTaskFunc()
 addTaskInput.addEventListener("keydown", (event) => {
@@ -102,17 +127,17 @@ const entryModalCont = document.querySelector(".entrymodal-cont");
 const firstName = document.querySelector(".first-name");
 const mainName = document.querySelector(".main-name");
 
-entryMainInp.setAttribute("Required", true);
-
 entryButton.addEventListener("click", entryButtonFunc);
+entryMainInp.value = " ";
 
 function entryButtonFunc() {
+  entryMainInp.setAttribute("required", true);
   entryMainInpValue = entryMainInp.value;
-  if (entryMainInpValue === " ") alert("Please, Enter your First name");
+  // entryMainInpValue.trim();
+  if (!entryMainInpValue) alert("Please, Enter your First name");
   else {
     firstName.textContent = entryMainInpValue + "!";
     mainName.textContent = entryMainInpValue;
-    entryMainInp.value = " ";
     entryModalCont.classList.add("active");
   }
 }
